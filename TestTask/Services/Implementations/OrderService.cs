51d43e39c4bd5cddddff03b25d6df1ec357e8c91
Tim_Pans_13 <95716918+chort13@users.noreply.github.com>
2023-10-03 18:12:14 +0300

    fix
    add try/catch

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+
+namespace TestTask.Services.Interfaces
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            try
+            {
+                var orderWithHighestTotalAmount = await _context.Orders
+                    .OrderByDescending(o => o.Price * o.Quantity)
+                    .FirstOrDefaultAsync();
+
+                return orderWithHighestTotalAmount;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred in GetOrder: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            try
+            {
+                var ordersWithQuantityGreaterThanTen = await _context.Orders
+                    .Where(o => o.Quantity > 10)
+                    .ToListAsync();
+
+                return ordersWithQuantityGreaterThanTen;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred in GetOrders: {ex.Message}");
+                throw; 
+            }
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+
+namespace TestTask.Services.Interfaces
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> GetUser()
+        {
+            try
+            {
+                var userWithMostOrders = await _dbContext.Users
+      .Where(u => u.Orders.Any())
+      .OrderByDescending(u => u.Orders.Count)
+      .Include(u => u.Orders)
+      .FirstOrDefaultAsync();
+
+                return userWithMostOrders;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred in GetUser: {ex.Message}");
+                throw;
+            }
+          
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            try
+            {
+                var inactiveUsers = await _dbContext.Users
+               .Where(u => u.Status == UserStatus.Inactive)
+               .ToListAsync();
+
+                return inactiveUsers;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred in GetUsers: {ex.Message}");
+                throw;
+            }
+           
+        }
+    }
+}
